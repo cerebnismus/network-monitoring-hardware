7132ebd7d3b2cb4041bd1f36ea5c6c83a2ca27ab
Oguzhan Ince <oguzhanlarca@pm.me>
2021-09-07 17:58:43 +0300

    Update snmpWalk-check.sh

--- snmpWalk-check.sh
+++ snmpWalk-check.sh
@@ -1,5 +1,5 @@
 for i in 'cat string_addr.txt'; do
-    if snmpwalk -v1 -c $i  &> /dev/null
+    if snmpwalk -v2 -c $i  &> /dev/null
     then echo "$i" >> snmp-ok   # SNMP OK
     then echo "$i" >> snmp-nok  # SNMP NOK
     fi
