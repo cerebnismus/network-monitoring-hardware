4bd6967814df10fb6a8551fc65be5996d6099298
Oguzhan Ince <oguzhanlarca@pm.me>
2021-09-10 19:33:48 +0300

    Delete icmp-check.sh

--- icmp-check.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for i in 'cat addr.txt'; do
-    if ping -c 1 $i &> /dev/null
-    then echo "$i" >> icmp-ok  # ICMP OK
-    else echo "$i" >> icmp-nok # ICMP NOK
-    fi
-done
