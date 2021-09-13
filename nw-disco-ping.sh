75aa3584569c15decde2c223487e61e28a6cd407
Oguzhan Ince <oguzhanlarca@pm.me>
2021-09-13 17:19:20 +0300

    Update nw-disco-ping.sh

--- nw-disco-ping.sh
+++ nw-disco-ping.sh
@@ -2,10 +2,16 @@
 
 cat ip.txt |  while read ip
 do
-    ping -c 1 "$ip" > /dev/null
+    ping -c1 -t1 -W5 "$ip" > /dev/null
     if [ $? -eq 0 ]; then
+    #echo "DOWN: $ip" 
     echo "$ip" >> up-output.txt
     else
+    #echo "UP: $ip" 
     echo "$ip" >> down-output.txt
     fi
+
+    echo "Total UP: " cat up-output.txt | wc -l 
+    echo "Total DOWN: " cat down-output.txt | wc -l 
+
 done
