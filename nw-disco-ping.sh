987c996c0bcd3d831077dcbd826d5a15b030a6cc
Oguzhan Ince <oguzhanlarca@pm.me>
2021-09-13 23:49:48 +0300

    percentage calculation

--- nw-disco-ping.sh
+++ nw-disco-ping.sh
@@ -1,16 +1,45 @@
 #!/bin/bash
 
-cat ip.txt |  while read ip
-do
-    ping -c1 -t1 -W5 "$ip" > /dev/null
+total=0
+reach=0
+unreach=0
+
+## SUBNETIZER ##
+cat ip.txt | while read ip; do
+  IFS=. read ip1 ip2 ip3 ip4 <<< "$ip"
+  echo $ip1\.$ip2 >> subnetizer.txt
+done
+
+sub=$(cat subnetizer.txt | sort | uniq)
+
+## PING ##
+cat ip.txt | while read ip; do
+    ping -c1 -t1 -W5 "$ip" >/dev/null
     if [ $? -eq 0 ]; then
-    echo "UP: $ip" 
-    echo "$ip" >> up-output.txt
+	echo "UP: $ip" 
+    	echo "$ip" >> up-output.txt
     else
-    echo "DOWN: $ip" 
-    echo "$ip" >> down-output.txt
+    	echo "DOWN: $ip" 
+    	echo "$ip" >> down-output.txt
     fi
 done
 
-echo "Total UP: " && cat up-output.txt | wc -l 
-echo "Total DOWN: " && cat down-output.txt | wc -l 
+export U="up-output.txt"
+export D="down-output.txt"
+
+reach=$(wc -l "$U" 2>/dev/null | awk '{print $1}')
+unreach=$(wc -l "$D" 2>/dev/null | awk '{print $1}')
+
+total=$((reach + unreach))
+perc=$((reach*100/total))
+
+## OUTPUT ##
+echo ""
+echo "Block     : $sub "
+echo "Total     : $total"
+echo "Total UP  : $reach"
+echo "Total DW  : $unreach"
+echo "Reachable : %$perc"
+echo ""
+
+rm subnetizer.txt up-output.txt down-output.txt &>/dev/null
