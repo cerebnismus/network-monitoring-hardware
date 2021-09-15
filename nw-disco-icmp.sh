7187c17d0407c86013bdcc4a3a67c6b3fef6379e
Oguzhan Ince <oguzhanlarca@pm.me>
2021-09-15 16:11:17 +0300

    Update nw-disco-icmp.sh

--- nw-disco-icmp.sh
+++ nw-disco-icmp.sh
@@ -3,6 +3,8 @@
 # get connection info from network eq.
 # (tryng fastest ping)
 
+## VARIABLES ##
+start="date"
 bold=$(tput bold)
 normal=$(tput sgr0)
 
@@ -10,42 +12,61 @@ total=0
 reach=0
 unreach=0
 
-## SUBNETIZER ##
-cat ip.txt | while read ip; do
-  IFS=. read ip1 ip2 ip3 ip4 <<< "$ip"
-  echo $ip1\.$ip2 >> subnetizer.txt
-done
-
-sub=$(cat subnetizer.txt | sort | uniq)
+## PROMPT ##
+echo -e "\nFILES:"
+ls | grep .txt
+echo -e -n "\nSpecify IP file: "
+read ans
 
 ## PING ##
 cat ip.txt | while read ip; do
-    ping -c1 -t1 -W5 "$ip" >/dev/null
+    ping -c1 -t50 -W5 "$ip" >/dev/null
     if [ $? -eq 0 ]; then
-	echo "UP: $ip" 
-    	echo "$ip" >> up-output.txt
+	echo "OK: $ip" 
+    	echo "$ip" >> icmp-ok-output.txt
     else
-    	echo "${bold}DOWN: $ip${normal}" 
-    	echo "$ip" >> down-output.txt
+    	echo "${bold}NOK: $ip${normal}" 
+    	echo "$ip" >> icmp-nok-output.txt
     fi
 done
 
-export U="up-output.txt"
-export D="down-output.txt"
+export OK="icmp-ok-output.txt"
+export NOK="icmp-nok-output.txt"
 
-reach=$(wc -l "$U" 2>/dev/null | awk '{print $1}')
-unreach=$(wc -l "$D" 2>/dev/null | awk '{print $1}')
+reach=$(wc -l "$OK" 2>/dev/null | awk '{print $1}')
+unreach=$(wc -l "$NOK" 2>/dev/null | awk '{print $1}')
 
 total=$((reach + unreach))
 perc=$((reach*100/total))
 
-## OUTPUT ##
-echo ""
-echo "Block     : $sub "
-echo "Total     : $total"
-echo "Total UP  : $reach"
-echo "Total DW  : $unreach"
-echo "Reachable : %$perc"
-echo ""
+## SUBNETIZER OK ##
+cat icmp-ok-output.txt | while read ip; do
+  IFS=. read ip1 ip2 ip3 ip4 <<< "$ip"
+  echo -e "$ip1\.$ip2\n" >> icmp-ok-subnetizer.txt
+done
 
-rm subnetizer.txt up-output.txt down-output.txt &>/dev/null
+oks=$(cat icmp-ok-output.txt | sort | uniq)
+echo -e "\nreachable blocks are:\n"
+echo $oks
+
+## SUBNETIZER NOK ##
+cat icmp-nok-output.txt | while read ip; do
+  IFS=. read ip1 ip2 ip3 ip4 <<< "$ip"
+  echo -e "$ip1\.$ip2\n" >> icmp-nok-subnetizer.txt
+done
+
+noks=$(cat icmp-nok-subnetizer.txt | sort | uniq)
+echo -e "\nunreachable blocks are:\n"
+echo $noks
+
+## OUTPUT ##
+echo -e "\nTotal     : $total"
+echo "Total OK  : $reach"
+echo "Total NOK : $unreach"
+echo "Reachable : %$perc"
+
+end="date"
+runtime=$((end-start))
+echo -e "Runtime   : $runtime\n"
+
+# rm icmp-ok-output.txt icmp-nok-output.txt icmp-ok-subnetizer.txt icmp-nok-subnetizer.txt &>/dev/null
