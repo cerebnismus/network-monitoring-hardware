5fc52fe9805fba349c21aa04121862910d09a622
Oguzhan Ince <oguzhan.ince@protonmail.com>
2023-01-08 05:48:07 +0300

    mac

--- practices_bash/getRedHatServerInfo.sh
+++ practices_bash/getRedHatServerInfo.sh
@@ -1,1 +1,67 @@
-clear && echo && echo ————————————————————————————— && echo '|' REDHAT SERVER INFORMATION '|' && echo ————————————————————————————— && uname -nr && uptime && echo && ifconfig -a eth0 | awk 'NR ==2 {print $2}' && ifconfig -a eth1 | awk 'NR ==2 {print $2}' && echo && dmidecode –type system | grep VMware- && echo && lscpu | grep 'BIOS Vendor ID:' && lscpu | grep 'BIOS Model name:' && lscpu | grep Architecture && lscpu | grep 'CPU MHz:' && lscpu | grep 'CPU(s):' && lscpu | grep 'Core(s) per socket:' && lscpu | grep 'Socket(s):' && cat /proc/meminfo | grep MemTotal && echo && echo
+#!/bin/bash
+# -*- coding: utf-8 -*-
+#  -*- cerebnismus -*-
+
+# get RedHat server information and print to console
+# 2018-01-30 useful for armada inventory entry 
+
+printf ' \n —————————————————————————————'
+printf ' \n | REDHAT SERVER INFORMATION |'
+printf ' \n —————————————————————————————'
+
+uptime1=$(uptime | cut -d, -f1)
+printf '\n\n -*-%s' "$uptime1"
+
+uname1=$(uname -nr)
+printf '\n -*- %s' "$uname1"
+
+
+ip1=$(ifconfig | grep 'inet ' | cut -d: -f2 | awk '{ print $2}')
+
+i=0
+printf '\n\n -*- Interfaces:'
+for ip in $ip1; do
+		let i+=1
+		printf '\n     IP%s: %s' "$i" "$ip"
+done
+
+# sudo permission required for dmidecode command
+# serial1=$(dmidecode -t system | grep 'VMware-')
+# echo "$serial1"
+ 
+lscpu1=$(lscpu | grep 'Vendor ID:')
+printf '\n\n -*- %s' "$lscpu1"
+
+lscpu2=$(lscpu | grep 'Model name:')
+printf '\n -*- %s' "$lscpu2"
+
+lscpu3=$(lscpu | grep 'Architecture')
+printf '\n -*- %s' "$lscpu3"
+
+lscpu4=$(lscpu | grep 'CPU MHz:')
+printf '\n -*- %s' "$lscpu4"
+
+lscpu5=$(lscpu | grep 'CPU(s):' | head -n 1)
+printf '\n -*- %s' "$lscpu5"
+
+lscpu6=$(lscpu | grep 'Core(s) per socket:')
+printf '\n -*- %s' "$lscpu6"
+
+lscpu7=$(lscpu | grep 'Socket(s):')
+printf '\n -*- %s' "$lscpu7"
+
+mem1=$(cat /proc/meminfo | grep MemTotal)
+printf '\n -*- %s' "$mem1"
+
+total_G=0
+disks_app_G=$(df -BG | grep ' /app' | awk '{ print $2}' | grep 'G' | cut -dG -f1)
+for g in $disks_app_G; do
+		let total_G+=$g
+done
+
+disks_home_G=$(df -BG | grep ' /home' | awk '{ print $2}' | grep 'G' | cut -dG -f1)
+for g in $disks_home_G; do
+		let total_G+=$g
+done
+
+printf '\n -*- DiskTotal:       %sG\n\n' "$total_G"
