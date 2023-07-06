9ce6e8b3975e1104c9ad1330f972528979109191
cerebnismus <oguzhan.ince@protonmail.com>
2023-07-07 02:31:30 +0300

    oguzhan.ince@raspberrypi

--- bop-gsm/bop-gsm.ino
+++ bop-gsm/bop-gsm.ino
@@ -1,36 +1,25 @@
-#include <SoftwareSerial.h>
+/*
+  btn_led_test.ino - This is example of testing button and led.
+  Created by Yasin Kaya (selengalp), September 11, 2018.
+*/
 
-// Set up the SoftwareSerial instance
-SoftwareSerial gsmSerial(7, 8);  // RX, TX pins for the GSM shield
+#include "
 
+SixfabTracker node;
+
+// setup
 void setup() {
-  // Start the serial communication
-  Serial.begin(9600);
-  gsmSerial.begin(9600);
-
-  // Read the phone number from the Raspberry Pi
-  char phone_number[20];
-  int index = 0;
-  while (Serial.available()) {
-    char c = Serial.read();
-    phone_number[index] = c;
-    index++;
-  }
-  phone_number[index] = '\0';  // Null-terminate the string
-
-  // Send the phone number to the GSM module
-  gsmSerial.print("AT+CMGS=\"");
-  gsmSerial.print(phone_number);
-  gsmSerial.println("\"");
-  delay(1000);
-
-  // Send an example SMS
-  gsmSerial.println("Hello from Arduino!");
-  delay(1000);
-  gsmSerial.write(26);  // End the SMS by sending Ctrl+Z
+  
+  node.init();
+  
+  node.turnOnUserLED();
+  delay(TIMEOUT);
+  node.turnOffUserLED();
+  delay(TIMEOUT);
+ 
 }
 
+// loop
 void loop() {
-  // Do nothing in the loop
-}
 
+}
