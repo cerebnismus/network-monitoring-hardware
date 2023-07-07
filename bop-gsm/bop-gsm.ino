c025cce2d794f6d63316ac0f43d09da6bb0de5f3
cerebnismus <oguzhan.ince@protonmail.com>
2023-07-07 03:39:46 +0300

    oguzhan.ince@raspberrypi

--- bop-gsm/bop-gsm.ino
+++ bop-gsm/bop-gsm.ino
@@ -1,31 +1,57 @@
 /*
   btn_led_test.ino - This is example of testing button and led.
   Created by Yasin Kaya (selengalp), September 11, 2018.
-  arduino-cli compile --fqbn arduino:avr:uno --port /dev/ttyUSB0 --libraries /home/pi/bowl-of-petunias/bop-gsm/libs/ --build-cache-path /home/pi/bowl-of-petunias/bop-gsm/build-cache/ --export-binaries --warnings all --output-dir /home/pi/bowl-of-petunias/bop-gsm/bin/ /home/pi/bowl-of-petunias/bop-gsm/bop-gsm.ino --upload --verify --verbose --clean
+
+Compile Parameters:
+arduino-cli compile  \
+  --fqbn arduino:avr:uno  \
+  --port /dev/ttyUSB0  \
+  --libraries /home/pi/bowl-of-petunias/bop-gsm/libs/  \
+  --build-cache-path /home/pi/bowl-of-petunias/bop-gsm/build-cache/  \
+  --export-binaries --warnings all  \
+  --output-dir /home/pi/bowl-of-petunias/bop-gsm/bin/  \
+  --upload  \
+  --verify  \
+  --verbose  \
+  --clean \
+  /home/pi/bowl-of-petunias/bop-gsm/bop-gsm.ino
 */
 
+#include <SoftwareSerial.h>
 #include "/home/pi/bowl-of-petunias/bop-gsm/libs/Sixfab_Tracker.h"
 #include "/home/pi/bowl-of-petunias/bop-gsm/libs/Sixfab_Tracker.cpp"
 
-SixfabTracker node;
+SixfabTracker tracker;
 
 void setup() {
   
-  node.init();
+  tracker.init();
   
-  node.turnOnUserLED();
+  tracker.turnOnUserLED();
   delay(1000);
-  node.turnOffUserLED();
+  tracker.turnOffUserLED();
   delay(1000);
-  node.turnOnUserLED();
+  tracker.turnOnUserLED();
   delay(1000);
-  node.turnOffUserLED();
+  tracker.turnOffUserLED();
   delay(1000);
-  node.turnOnUserLED();
+  tracker.turnOnUserLED();
   delay(1000);
+
+  // Make sure the module is connected to the network
+  if (tracker.isNetworkConnected()) {
+    // Send an SMS
+    tracker.sendSMS("+905304543426", "BOP!");
+
+    // Print a message to indicate that the SMS was sent
+    DEBUG.println("SMS sent successfully!");
+  } else {
+    // Print a message to indicate that the module is not connected to the network
+    DEBUG.println("Network is not connected!");
+  }
 }
 
-// loop
+
 void loop() {
 
 }
