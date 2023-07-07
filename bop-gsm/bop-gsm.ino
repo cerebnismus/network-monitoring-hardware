f265e8f054b7990b90e63e16f1f19d7c8f1844f8
cerebnismus <oguzhan.ince@protonmail.com>
2023-07-07 03:25:16 +0300

    oguzhan.ince@raspberrypi

--- bop-gsm/bop-gsm.ino
+++ bop-gsm/bop-gsm.ino
@@ -1,22 +1,28 @@
 /*
   btn_led_test.ino - This is example of testing button and led.
   Created by Yasin Kaya (selengalp), September 11, 2018.
+  arduino-cli compile --fqbn arduino:avr:uno --port /dev/ttyUSB0 --libraries /home/pi/bowl-of-petunias/bop-gsm/libs/ --build-cache-path /home/pi/bowl-of-petunias/bop-gsm/build-cache/ --export-binaries --warnings all --output-dir /home/pi/bowl-of-petunias/bop-gsm/bin/ /home/pi/bowl-of-petunias/bop-gsm/bop-gsm.ino --upload --verify --verbose --clean
 */
 
-#include "
+#include "/home/pi/bowl-of-petunias/bop-gsm/libs/Sixfab_Tracker.h"
+#include "/home/pi/bowl-of-petunias/bop-gsm/libs/Sixfab_Tracker.cpp"
 
 SixfabTracker node;
 
-// setup
 void setup() {
   
   node.init();
   
   node.turnOnUserLED();
-  delay(TIMEOUT);
+  delay(1000);
   node.turnOffUserLED();
-  delay(TIMEOUT);
- 
+  delay(1000);
+  node.turnOnUserLED();
+  delay(1000);
+  node.turnOffUserLED();
+  delay(1000);
+  node.turnOnUserLED();
+  delay(1000);
 }
 
 // loop
