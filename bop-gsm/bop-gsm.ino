485c4732ef3475b472c5edabf1668a2bf1d888cf
cerebnismus <oguzhan.ince@protonmail.com>
2023-07-07 16:39:39 +0300

    oguzhan.ince@raspberrypi

--- bop-gsm/bop-gsm.ino
+++ bop-gsm/bop-gsm.ino
@@ -3,6 +3,11 @@
   Created by Yasin Kaya (selengalp), September 11, 2018.
   Updated by Oguzhan Ince (cerebnismus), July 07, 2023.
 
+
+arduino-cli board list
+arduino-cli lib install "Sixfab_Tracker"
+arduino-cli lib install "SoftwareSerial@1.0.0"
+
 Compile Parameters:
 arduino-cli compile  \
   --fqbn arduino:avr:uno  \
@@ -44,9 +49,34 @@ void setup() {
   tracker.sendSMS("+905304543426", "BOP2!");
   DEBUG.println("SMS sent!");
 
+  // AT command to configure GSM module for text mode SMS
+  sendATCommand("AT+CMGF=1\r\n", 1000);
+
+
 }
 
 
 void loop() {
+  if (Serial.available()) {
+    char input = Serial.read();
+    if (input == 'p') {
+      icmpPing();
+    }
+  }
+}
 
+void sendATCommand(String command, int delayTime) {
+  gsmSerial.print(command);
+  delay(delayTime);
+  while (gsmSerial.available()) {
+    response = gsmSerial.readString();
+    Serial.print(response);
+  }
+}
+
+
+void icmpPing() {
+  // AT command to send ICMP Echo Request to the target IP
+  String command = "AT+PING=\"192.168.8.106\"\r\n";
+  sendATCommand(command, 5000);
 }
